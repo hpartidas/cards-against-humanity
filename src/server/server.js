fd70e5d62d83d69c386eaec8a1f949b78da650ff
Hector Partidas <hpartidas@deuz.net>
2016-11-12 16:07:09 -0500

    Gulp Install & Config

--- src/server/server.js
+++ src/server/server.js
@@ -2,5 +2,13 @@ const foo = "bar";
 console.log(`hello ${foo}`);
 
 const obj = {hey: 1};
-const obj2 = {...obj, foo: 2}
+const obj2 = {...obj, foo: 2};
 console.log(obj2);
+
+class AppComponent {
+    static PropTypes = {
+        foo: "bar"
+    };
+}
+
+console.log(<AppComponent/>);
