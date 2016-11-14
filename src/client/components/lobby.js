c95ad25657735aa3bcc8bba6c93e15219b35d323
Hector Partidas <hpartidas@deuz.net>
2016-11-14 17:14:27 -0500

    Implemented Lobby Implemented Chat

--- src/client/components/lobby.js
+++ src/client/components/lobby.js
@@ -1,17 +1,104 @@
 import "./lobby.scss";
 
 import React, {Component} from "react";
+import Chat from "./chat";
 
 class LobbyContainer extends Component {
+    constructor (props) {
+        super(props);
+        this._joinGame = (game) => {
+            console.log(`TODO: JOIN GAME ${game.title}`);
+        };
+
+        this._sendMessage = (message) => {
+            console.log(`Sending ${message}`);
+        };
+    }
     render() {
-        return <p>Lobby!</p>;
+        const games = [
+            {title: "Game 1", id: 1, players: ["one", "two", "three"]},
+            {title: "Game 2", id: 2, players: ["one", "two", "three"]},
+            {title: "Game 3", id: 3, players: ["one", "two", "three"]},
+            {title: "Game 4", id: 4, players: ["one", "two", "three"]},
+        ];
+
+        const opSendMessage = {can: true, inProgress: false};
+        const messages = [
+            {index: 1, name: "Person", message: "bleh"},
+            {index: 2, name: "Person", message: "bleh, bleh"},
+            {index: 3, name: "Dracula", message: "bleh, bleh, bleh, bleh"}
+        ];
+
+        return (
+            <div className="c-lobby">
+                <GameList games={games} joinGame={this._joinGame} />
+                <Chat
+                    messages={messages}
+                    opSendMessage={opSendMessage}
+                    sendMessage={this._sendMessage}
+                />
+            </div>
+        );
     }
 }
 
 class LobbySidebar extends Component {
-    render() {
-        return <p>Lobby Sidebar</p>;
+    constructor (props) {
+        super(props);
+
+        this._login = () => {
+            console.log("TODO: Implement Login");
+        };
+
+        this._createGame = () => {
+            console.log("TODO: CREATE Game");
+        };
     }
+
+    render() {
+        const canLogin = true;
+        const canCreateGame = true;
+        const createGameInProgress = false;
+
+        return (
+            <section className="c-lobby-sidebar">
+                <div className="m-sidebar-buttons">
+                    {
+                        !canLogin
+                            ? null
+                            : <button className="m-button primary" onClick={this._login}>Login</button>
+                    }
+                    {
+                        !canCreateGame
+                        ? null
+                        : <button
+                            onClick={this._createGame}
+                            disabled={createGameInProgress}
+                            className="m-button good">
+                            Create Game
+                        </button>
+                    }
+                </div>
+            </section>
+        );
+    }
+}
+
+function GameList({games, joinGame}) {
+    return (
+        <section className="c-game-list">
+            {games.length > 0 ? null : <div className="no-games">There are no games yet :(</div> }
+            {games.map(game =>
+                <div className="game" key={game.id} onClick={() => joinGame(game)}>
+                    <div className="title">{game.title}</div>
+                    <div className="players">
+                        {game.players.join(", ")}
+                    </div>
+                    <div className="join-game">Join Game</div>
+                </div>
+            )}
+        </section>
+    );
 }
 
 export default {
